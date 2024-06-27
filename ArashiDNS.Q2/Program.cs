16b5aaf175b84a997d61aec4aab7edce85c78f6c
Milkey Tan <24996957+mili-tan@users.noreply.github.com>
2024-06-27 20:15:08 +0800

    cmd

--- ArashiDNS.Q2/Program.cs
+++ ArashiDNS.Q2/Program.cs
@@ -4,13 +4,14 @@ using System.Net.Security;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace ArashiDNS.Q2
 {
     internal class Program
     {
+        public static IPEndPoint ListenerEndPoint = new(IPAddress.Any, 853);
         public static IPAddress UpStream = IPAddress.Parse("8.8.8.8");
-        public static int Port = 853;
         public static int Timeout = 1000;
         public static X509Certificate2Collection Certificate2Collection = new();
 
@@ -22,6 +23,26 @@ namespace ArashiDNS.Q2
 
         static async Task Main(string[] args)
         {
+            var cmd = new CommandLineApplication
+            {
+                Name = "ArashiDNS.KS",
+                Description = "ArashiDNS.KS - DNS over KCP Server" +
+                              Environment.NewLine +
+                              $"Copyright (c) {DateTime.Now.Year} Milkey Tan. Code released under the MPL License"
+            };
+            cmd.HelpOption("-?|-h|--help");
+            var isZh = Thread.CurrentThread.CurrentCulture.Name.Contains("zh");
+            var upArgument = cmd.Argument("target",
+                isZh ? "目标上游 DNS 端点" : "Target upstream DNS service endpoint");
+            var ipOption = cmd.Option<string>("-l|--listen <IPEndPoint>",
+                isZh ? "监听的地址与端口" : "Set server listening address and port", CommandOptionType.SingleValue);
+            var pemOption = cmd.Option<string>("-p|--pem <Path>",
+                isZh ? "PEM 证书文件路径" : "PEM certificate file path", CommandOptionType.SingleValue);
+            var keyOption = cmd.Option<string>("-k|--key <Path>",
+                isZh ? "私钥文件路径" : "Private key file path", CommandOptionType.SingleValue);
+            var wOption = cmd.Option<int>("-w <timeout>",
+                isZh ? "等待回复的超时时间(毫秒)。" : "Timeout time to wait for reply", CommandOptionType.SingleValue);
+
             if (args.Any(x => x.StartsWith("--crts")))
             {
                 foreach (var item in args.FirstOrDefault(x => x.StartsWith("--crts="))?.Split("=").LastOrDefault()?.Split(',')!)
@@ -30,55 +51,62 @@ namespace ArashiDNS.Q2
                         .GetStringAsync(item).Result));
                 }
             }
-            if (args.Any(x => x.StartsWith("--port=")))
-                Port = int.Parse(args.FirstOrDefault(x => x.StartsWith("--port="))?.Split("=").LastOrDefault() ?? "853");
 
-            var pem = args.FirstOrDefault(x => x.StartsWith("--pem="))?.Split("=").LastOrDefault();
-            var key = args.FirstOrDefault(x => x.StartsWith("--key="))?.Split("=").LastOrDefault();
-            var cert = X509Certificate2.CreateFromPem(pem, key);
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                cert = new X509Certificate2(cert.Export(X509ContentType.Pfx), (string?)null, X509KeyStorageFlags.Exportable);
-
-            var sslOptions = new SslServerAuthenticationOptions()
+            cmd.OnExecute(async () =>
             {
-                ApplicationProtocols = QuicApplicationProtocols,
-                ServerCertificateContext = SslStreamCertificateContext.Create(cert, Certificate2Collection),
-                ClientCertificateRequired = false,
-            };
-            var listenerOptions = new QuicListenerOptions()
-            {
-                ListenEndPoint = new IPEndPoint(IPAddress.Any, Port),
-                ListenBacklog = 100,
-                ApplicationProtocols = QuicApplicationProtocols,
-                ConnectionOptionsCallback = delegate
+                if (upArgument.HasValue) UpStream = IPEndPoint.Parse(upArgument.Value!).Address;
+                if (ipOption.HasValue()) ListenerEndPoint = IPEndPoint.Parse(ipOption.Value()!);
+                if (wOption.HasValue()) Timeout = int.Parse(wOption.Value()!);
+
+                var pem = await File.ReadAllTextAsync(pemOption.Value() ?? "crt.pem");
+                var key = await File.ReadAllTextAsync(keyOption.Value() ?? "key.key");
+                var cert = X509Certificate2.CreateFromPem(pem, key);
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    cert = new X509Certificate2(cert.Export(X509ContentType.Pfx), (string?)null, X509KeyStorageFlags.Exportable);
+
+                var sslOptions = new SslServerAuthenticationOptions()
                 {
-                    var serverConnectionOptions = new QuicServerConnectionOptions()
+                    ApplicationProtocols = QuicApplicationProtocols,
+                    ServerCertificateContext = SslStreamCertificateContext.Create(cert, Certificate2Collection),
+                    ClientCertificateRequired = false,
+                };
+                var listenerOptions = new QuicListenerOptions()
+                {
+                    ListenEndPoint = ListenerEndPoint,
+                    ListenBacklog = 100,
+                    ApplicationProtocols = QuicApplicationProtocols,
+                    ConnectionOptionsCallback = delegate
                     {
-                        DefaultCloseErrorCode = 0,
-                        DefaultStreamErrorCode = 5,
-                        MaxInboundUnidirectionalStreams = 0,
-                        MaxInboundBidirectionalStreams = 100,
-                        IdleTimeout = TimeSpan.FromMilliseconds(1000),
-                        ServerAuthenticationOptions = sslOptions
-                    };
-                    return ValueTask.FromResult(serverConnectionOptions);
+                        var serverConnectionOptions = new QuicServerConnectionOptions()
+                        {
+                            DefaultCloseErrorCode = 0,
+                            DefaultStreamErrorCode = 5,
+                            MaxInboundUnidirectionalStreams = 0,
+                            MaxInboundBidirectionalStreams = 100,
+                            IdleTimeout = TimeSpan.FromMilliseconds(1000),
+                            ServerAuthenticationOptions = sslOptions
+                        };
+                        return ValueTask.FromResult(serverConnectionOptions);
+                    }
+                };
+
+                var listener = await QuicListener.ListenAsync(listenerOptions);
+                for (var i = 0; i < 4; i++) await Task.Factory.StartNew(() => AcceptQuicConnection(listener));
+                Console.WriteLine("Application started. Press Ctrl+C / q to shut down.");
+
+                if (!Console.IsInputRedirected && Console.KeyAvailable)
+                {
+                    while (true)
+                        if (Console.ReadKey().KeyChar == 'q')
+                            Environment.Exit(0);
                 }
-            };
 
-            var listener = await QuicListener.ListenAsync(listenerOptions);
-            for (var i = 0; i < 4; i++) await Task.Factory.StartNew(() => AcceptQuicConnection(listener));
-            Console.WriteLine("Application started. Press Ctrl+C / q to shut down.");
+                EventWaitHandle wait = new AutoResetEvent(false);
+                while (true) wait.WaitOne();
+            });
 
-            if (!Console.IsInputRedirected && Console.KeyAvailable)
-            {
-                while (true)
-                    if (Console.ReadKey().KeyChar == 'q')
-                        Environment.Exit(0);
-            }
-
-            EventWaitHandle wait = new AutoResetEvent(false);
-            while (true) wait.WaitOne();
+            cmd.Execute(args);
         }
 
         static async Task AcceptQuicConnection(QuicListener quicListener)
